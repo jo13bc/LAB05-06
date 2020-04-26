309823c69eea07ccf295c7a88b8fdbadd870de31
Mm1208 <41961128+Mm1208@users.noreply.github.com>
2020-04-26 12:49:43 -0600

    Admin y usuario

---
+++ Login/app/src/main/java/com/miker/login/NavDrawerActivityAdmin.java
@@ -0,0 +1,121 @@
+package com.miker.login;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+//import com.miker.login.carrera.CarrerasActivity; formulario
+
+public class NavDrawerActivityAdmin extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private SharedPreferences mPrefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer_admin);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_admin);
+        setSupportActionBar(toolbar);
+
+        mPrefs = this.getSharedPreferences(getString(R.string.preference_user_key), Context.MODE_PRIVATE);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_admin);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_admin);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view_admin);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_admin);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer_activity_admin, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings_admin) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        Gson gson = new Gson();
+        String json = mPrefs.getString(getString(R.string.preference_user_key), "");
+        Model model = gson.fromJson(json, Model.class);
+        //     int privilegio = model.getLoggedUser().;
+        //    Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_home) {
+            Intent intent = new Intent(NavDrawerActivityAdmin.this, NavDrawerActivityAdmin.class);
+             NavDrawerActivityAdmin.this.startActivity(intent);
+            // Handle the camera action
+        } else if (id == R.id.nav_buscar_admin) {
+            Intent  intent = new Intent(NavDrawerActivityAdmin.this, ListActivity.class);
+           NavDrawerActivityAdmin.this.startActivity(intent);
+        } else if (id == R.id.nav_button_admin) {
+            Intent intent = new Intent(NavDrawerActivityAdmin.this, ListActivity.class);
+           NavDrawerActivityAdmin.this.startActivity(intent);
+        }  else if (id == R.id.nav_logout_admin) {
+            finish();
+            Intent  intent = new Intent(NavDrawerActivityAdmin.this, MainActivity.class);
+             NavDrawerActivityAdmin.this.startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout_admin);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
